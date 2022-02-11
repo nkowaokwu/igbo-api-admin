c46990c0e327bc1732d9e79299a66fe8c0d81606
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-02-10 20:45:10 -0500

    Mock Firebase Firestore for Jest tests

--- __mocks__/firebase.ts
+++ __mocks__/firebase.ts
@@ -4,6 +4,12 @@ firebase.analytics = jest.fn().mockImplementation();
 firebase.auth = jest.fn(() => ({}));
 firebase.functions = jest.fn(() => ({
   httpsCallable: jest.fn(() => () => ({})),
+  useEmulator: jest.fn(() => ({})),
+}));
+firebase.firestore = jest.fn(() => ({
+  db: {
+    settings: jest.fn(() => ({})),
+  }
 }));
 firebase.auth.GoogleAuthProvider = jest.fn().mockImplementation();
 firebase.auth.FacebookAuthProvider = jest.fn().mockImplementation();
