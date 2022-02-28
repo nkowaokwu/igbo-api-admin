81d4c49a04e867dbd77b5503bdf9427cb43621f9
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-02-28 09:46:56 -0500

    Save redirection url after being logged out

--- src/utils/authProvider.ts
+++ src/utils/authProvider.ts
@@ -1,12 +1,25 @@
 import { FirebaseAuthProvider } from 'react-admin-firebase';
-import useFirebaseConfig from '../hooks/useFirebaseConfig';
+import { omit } from 'lodash';
+import useFirebaseConfig from 'src/hooks/useFirebaseConfig';
+import LocalStorageKeys from 'src/shared/constants/LocalStorageKeys';
 
+const LOGIN_HASH = '#/login';
 const firebaseConfig = useFirebaseConfig();
 const firebaseAuthProvider = FirebaseAuthProvider(firebaseConfig, { lazyLoading: { enabled: true }, logging: false });
 export default {
   ...firebaseAuthProvider,
-  logout: (args) => {
-    window.localStorage.clear();
+  getPermissions: async (): Promise<any> => {
+    // @ts-expect-error
+    const result = await firebaseAuthProvider.getPermissions();
+    if (!result && window.location.hash !== LOGIN_HASH) {
+      localStorage.setItem(LocalStorageKeys.REDIRECT_URL, window.location.hash);
+    }
+    return result;
+  },
+  logout: (args: any): Promise<string | void> => {
+    Object.keys(omit(LocalStorageKeys, ['REDIRECT_URL'])).forEach((key) => {
+      localStorage.removeItem(LocalStorageKeys[key]);
+    });
     return firebaseAuthProvider.logout(args);
   },
 };
