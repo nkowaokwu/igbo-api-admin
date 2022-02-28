203987178be74ee682a932f76ae57a7b022da46f
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-02-28 18:10:04 -0500

    Set redirection URL in logout authProvider function

--- src/utils/authProvider.ts
+++ src/utils/authProvider.ts
@@ -8,15 +8,10 @@ const firebaseConfig = useFirebaseConfig();
 const firebaseAuthProvider = FirebaseAuthProvider(firebaseConfig, { lazyLoading: { enabled: true }, logging: false });
 export default {
   ...firebaseAuthProvider,
-  getPermissions: async (): Promise<any> => {
-    // @ts-expect-error
-    const result = await firebaseAuthProvider.getPermissions();
-    if (!result && window.location.hash !== LOGIN_HASH) {
+  logout: (args: any): Promise<string | void> => {
+    if (window.location.hash !== LOGIN_HASH) {
       localStorage.setItem(LocalStorageKeys.REDIRECT_URL, window.location.hash);
     }
-    return result;
-  },
-  logout: (args: any): Promise<string | void> => {
     Object.keys(omit(LocalStorageKeys, ['REDIRECT_URL'])).forEach((key) => {
       localStorage.removeItem(LocalStorageKeys[key]);
     });
