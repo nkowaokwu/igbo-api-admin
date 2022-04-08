8d58d93926be2fd18f6372fa0918e8e9ffb389d4
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-04-08 12:14:03 -0400

    Safeguard WordClassTextField

--- src/shared/components/WordClassTextField.tsx
+++ src/shared/components/WordClassTextField.tsx
@@ -1,9 +1,10 @@
 import React, { ReactElement } from 'react';
+import { get } from 'lodash';
 import WordClass from '../constants/WordClass';
 import { WordClassTextFieldProps } from '../interfaces';
 
 const WordClassTextField = ({ record, source }: WordClassTextFieldProps): ReactElement => (
-  <span>{WordClass[record[source]]?.label || `${record[source]} [UPDATE PART OF SPEECH]`}</span>
+  <span>{get(WordClass[get(record, source)], 'label') || `${get(record, source)} [UPDATE PART OF SPEECH]`}</span>
 );
 
 export default WordClassTextField;
