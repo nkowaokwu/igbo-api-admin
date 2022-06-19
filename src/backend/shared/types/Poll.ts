1aac789f60b43043fb12fcd851c4ceb63523d391
ijemmao <ijemmaonwuzulike10@gmail.com>
2022-06-18 21:09:05 -0400

    Support up to four poll options

--- src/backend/shared/types/Poll.ts
+++ src/backend/shared/types/Poll.ts
@@ -1,7 +1,4 @@
 export type Poll = {
-  constructedTerm: string,
-  englishTerm: string,
-  definition: string,
   text: string,
   poll: {
     options: string[],
--- src/shared/components/views/creates/PollsCreate.tsx
+++ src/shared/components/views/creates/PollsCreate.tsx
@@ -1,47 +1,99 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ChangeEvent, ReactElement, useState } from 'react';
 import {
   Box,
   Button,
   Heading,
+  IconButton,
   Input,
   Link,
   Text,
   chakra,
   Tooltip,
+  Textarea,
+  useToast,
 } from '@chakra-ui/react';
+import { AddIcon, DeleteIcon } from '@chakra-ui/icons';
 import ConstructedPollThread from 'src/backend/shared/constants/ConstructedPollThread';
 import { submitConstructedTermPoll } from 'src/shared/API';
 import { CONSTRUCTED_TERMS_REVIEW_DOC } from 'src/Core/constants';
 import Tweet from './components/Tweet';
 
+const MINIMUM_POLL_OPTIONS = 2;
+const MAXIMUM_POLL_OPTIONS = 4;
 const PollsCreate = (): ReactElement => {
-  const [constructedTerm, setConstructedTerm] = useState('');
-  const [englishTerm, setEnglishTerm] = useState('');
-  const [definition, setDefinition] = useState('');
+  const [tweetBody, setTweetBody] = useState('');
+  const [pollOptions, setPollOptions] = useState(['Yes', 'No']);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const toast = useToast();
 
-  const handlePostingPoll = (e) => {
+  const determinePollPosition = (index: number) => {
+    if (index === 0) {
+      return 'First';
+    }
+    if (index === 1) {
+      return 'Second';
+    }
+    if (index === 2) {
+      return 'Third';
+    }
+    if (index === 3) {
+      return 'Fourth';
+    }
+    return null;
+  };
+
+  const handlePostingPoll = async (e) => {
     e.preventDefault();
     try {
       setIsSubmitting(true);
-      const initialTweet = ConstructedPollThread[0].text
-        .replaceAll('{constructedTerm}', constructedTerm)
-        .replaceAll('{englishTerm}', englishTerm)
-        .replaceAll('{definition}', definition);
+      if (pollOptions.some((pollOption) => !pollOption)) {
+        throw new Error('Unable to send poll with empty options');
+      }
+      const initialTweet = `${ConstructedPollThread[0].text} ${tweetBody}`;
       const poll = {
-        constructedTerm,
-        englishTerm,
-        definition,
         text: initialTweet,
         poll: {
-          options: ['Yes', 'No'],
+          options: pollOptions,
           duration_minutes: 4320,
         },
       };
-      submitConstructedTermPoll(poll);
+      await submitConstructedTermPoll(poll);
+      toast({
+        title: 'Success',
+        description: 'Sent the Twitter poll',
+        status: 'success',
+        duration: 4000,
+        isClosable: true,
+      });
+    } catch (err) {
+      toast({
+        title: 'Error',
+        description: err.message,
+        status: 'error',
+        duration: 4000,
+        isClosable: true,
+      });
     } finally {
       setIsSubmitting(false);
     }
+  };
+
+  const handleAddPollOption = () => {
+    if (pollOptions.length < MAXIMUM_POLL_OPTIONS) {
+      setPollOptions(pollOptions.concat(['']));
+    }
+  };
+
+  const handleDeletePollOption = (index: number) => () => {
+    const updatedPollOptions = [...pollOptions];
+    updatedPollOptions.splice(index, 1);
+    setPollOptions(updatedPollOptions);
+  };
+
+  const handleUpdatePollOption = (index: number) => (e: ChangeEvent) => {
+    const updatedPollOptions = [...pollOptions];
+    updatedPollOptions[index] = e.target.value;
+    setPollOptions(updatedPollOptions);
   };
 
   const handleAccountAuthorization = () => {
@@ -92,40 +144,61 @@ const PollsCreate = (): ReactElement => {
         >
           <Box flex={2} className="w-full space-y-4">
             <Box>
-              <Heading as="h3" fontSize="md" mb={4}>Constructed Term</Heading>
-              <Input
+              <Heading as="h3" fontSize="md" mb={4}>Tweet Body</Heading>
+              <Textarea
                 required
-                name="constructedTerm"
-                placeholder="Newly constructed Igbo term"
-                defaultValue={constructedTerm}
-                onChange={(e) => setConstructedTerm(e.target.value)}
+                name="tweetBody"
+                placeholder="Body of the main Tweet"
+                defaultValue={tweetBody}
+                onChange={(e) => setTweetBody(e.target.value)}
               />
             </Box>
             <Box>
-              <Heading as="h3" fontSize="md" mb={4}>English equivalent</Heading>
-              <Input
-                required
-                name="englishTerm"
-                placeholder="Constructed term's English equivalent"
-                defaultValue={englishTerm}
-                onChange={(e) => setEnglishTerm(e.target.value)}
-              />
-            </Box>
-            <Box>
-              <Heading as="h3" fontSize="md" mb={4}>Definition</Heading>
-              <Input
-                required
-                name="definition"
-                placeholder="Constructed term definition"
-                defaultValue={definition}
-                onChange={(e) => setDefinition(e.target.value)}
-              />
+              <Box className="flex flex-row justify-between items-center" mb={4}>
+                <Heading as="h3" fontSize="md">Poll Options</Heading>
+                {pollOptions.length < MAXIMUM_POLL_OPTIONS ? (
+                  <IconButton
+                    colorScheme="green"
+                    aria-label="Add poll option"
+                    icon={<AddIcon />}
+                    onClick={handleAddPollOption}
+                  />
+                ) : null}
+              </Box>
+              {pollOptions.map((option, index) => (
+                <Box mb={3}>
+                  <Text fontWeight="bold">{`${determinePollPosition(index)} option:`}</Text>
+                  <Box className="flex flex-row justify-between items-center">
+                    <Input
+                      placeholder={`${determinePollPosition(index)} option`}
+                      value={option}
+                      onChange={handleUpdatePollOption(index)}
+                    />
+                    {pollOptions.length > MINIMUM_POLL_OPTIONS ? (
+                      <Tooltip label="Delete poll option">
+                        <IconButton
+                          ml={4}
+                          color="red"
+                          aria-label="Delete poll option"
+                          icon={<DeleteIcon />}
+                          onClick={handleDeletePollOption(index)}
+                        />
+                      </Tooltip>
+                    ) : null}
+                  </Box>
+                </Box>
+              ))}
+
             </Box>
           </Box>
           <Box flex={3} className="w-full">
             <Heading as="h3" fontSize="md" mb={4}>Final Twitter thread</Heading>
-            {ConstructedPollThread.map(({ text }) => (
-              <Tweet text={text} constructedTerm={constructedTerm} englishTerm={englishTerm} definition={definition} />
+            {ConstructedPollThread.map(({ text }, index) => (
+              <Tweet
+                text={text}
+                {...(index === 0 ? { tweetBody } : {})}
+                {...(index === 0 ? { pollOptions } : {})}
+              />
             ))}
           </Box>
         </Box>
