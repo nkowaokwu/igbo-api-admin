85ba1ff62ac967abc0583719ee16732ce08a68ca
Ijemma Onwuzulike <ijemmaonwuzulike@Ijemmas-MBP.fios-router.home>
2022-10-08 01:08:42 -0400

    Send POST request to send Slack bot message about new Twitter post

--- src/shared/components/TweetField.tsx
+++ src/shared/components/TweetField.tsx
@@ -1,15 +1,14 @@
 import React, { ReactElement } from 'react';
 import { get } from 'lodash';
 import { Box, Link } from '@chakra-ui/react';
+import { TWITTER_APP_URL } from 'src/Core/constants';
 import { IdFieldProps } from '../interfaces';
-
-const TWITTER_ROOT = 'https://twitter.com/nkowaokwu/status';
 
 const TweetField = ({ source, record = {} }: IdFieldProps): ReactElement => (
   <Box display="flex" flexDirection="row" alignItems="center" className="space-x-4">
     <Link
       className="MuiTypography-root MuiTypography-body"
-      href={`${TWITTER_ROOT}/${get(record, source)}`}
+      href={`${TWITTER_APP_URL}/${get(record, source)}`}
       color="green.600"
       textDecoration="underline"
       target="_blank"
