871dd3c2b0a277664eb660d7df0aaf8d46629ca7
Robert Orazu <orazurobert@yahoo.com>
2022-10-25 02:22:58 +0100

    Updated and re-enableb the cypress test suite

---
+++ cypress.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 30000,
+  requestTimeout: 10000,
+  retries: 1,
+  projectId: 'dfw64t',
+  screenshotOnRunFailure: false,
+  video: false,
+  videoUploadOnPasses: false,
+  viewportHeight: 960,
+  viewportWidth: 1536,
+  numTestsKeptInMemory: 2,
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:3030',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+    },
+  },
+});
