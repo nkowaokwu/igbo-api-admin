f8fc11f6187b1e7bf0fc9ae772c5d0abc919b1a1
Ijemma Onwuzulike <ijemmaonwuzulike@Ijemmas-MBP.fios-router.home>
2022-11-16 14:07:30 -0500

    Fix getting stats for documents and update audio recorder

--- src/hooks/useRecorder.ts
+++ src/hooks/useRecorder.ts
@@ -1,17 +1,19 @@
 /* From: https://codesandbox.io/s/81zkxw8qnl?file=/src/useRecorder.js:370-385 */
 /* From: https://medium.com/front-end-weekly/recording-audio-in-mp3-using-reactjs-under-5-minutes-5e960defaf10 */
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useToast } from '@chakra-ui/react';
 import Wave from 'wave-visualizer';
 import MicRecorder from 'mic-recorder-to-mp3';
 
 const MAX_AUDIO_SIZE = 1000000;
-const useRecorder = (): [string, boolean, () => void, () => void] => {
+const useRecorder = (): [string, boolean, () => void, () => void, number] => {
   const [wave] = useState(new Wave());
   const [audioBlob, setAudioBlob] = useState('');
   const [isRecording, setIsRecording] = useState(false);
   const [isBlocked, setIsBlocked] = useState(true);
   const [recorder, setRecorder] = useState(null);
+  const [recordingDuration, setRecordingDuration] = useState(0);
+  const durationRef = useRef<NodeJS.Timer>();
   const toast = useToast();
 
   useEffect(() => {
@@ -29,7 +31,7 @@ const useRecorder = (): [string, boolean, () => void, () => void] => {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
         try {
           wave.fromStream(stream, 'canvas', {
-            colors: ['red', 'white', 'blue'],
+            colors: ['#777777'],
           });
           wave.fromElement('audio', 'canvas');
         } catch (err) {
@@ -43,6 +45,10 @@ const useRecorder = (): [string, boolean, () => void, () => void] => {
     if (!isBlocked && recorder) {
       recorder.start().then(() => {
         setIsRecording(true);
+        setRecordingDuration(0);
+        durationRef.current = setInterval(() => {
+          setRecordingDuration((prevRecordingDuration) => prevRecordingDuration + 1);
+        }, 1000);
       });
     }
   };
@@ -50,12 +56,10 @@ const useRecorder = (): [string, boolean, () => void, () => void] => {
   const stopRecording = () => {
     if (!isBlocked && recorder) {
       recorder.stop().getMp3().then(([buffer, blob]) => {
-        // @ts-expect-error File
         const file = new File(buffer, 'me-at-thevoice.mp3', {
           type: blob.type,
           lastModified: Date.now(),
         });
-        // @ts-expect-error FileReader
         const reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onloadend = (e): void | React.ReactText => {
@@ -83,11 +87,14 @@ const useRecorder = (): [string, boolean, () => void, () => void] => {
           return setAudioBlob(reader.result);
         };
         setIsRecording(false);
+        if (durationRef.current) {
+          clearInterval(durationRef.current);
+        }
       });
     }
   };
 
-  return [audioBlob, isRecording, startRecording, stopRecording];
+  return [audioBlob, isRecording, startRecording, stopRecording, recordingDuration];
 };
 
 export default useRecorder;
