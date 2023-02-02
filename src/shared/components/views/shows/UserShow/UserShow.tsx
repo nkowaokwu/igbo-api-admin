cca04e87a027a62dfae3317d3ce7a9e9203bb0f0
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-02-02 09:00:04 -0500

    Fix small info icon bug

--- src/shared/components/views/shows/UserShow/UserShow.tsx
+++ src/shared/components/views/shows/UserShow/UserShow.tsx
@@ -56,14 +56,13 @@ const UserShow = (props: ShowProps): ReactElement => {
   return (
     <Skeleton isLoaded={!isLoading}>
       <Box className="bg-white shadow-sm p-10 mt-10">
-        <Box className="flex flex-col lg:flex-row items-center text-center lg:text-left space-y-4 lg:space-x-4">
+        <Box className="flex flex-col lg:flex-row items-center text-center lg:text-left space-y-4 lg:space-x-4 mb-4">
           <Avatar name={displayName} src={photoURL} size="xl" />
           <Box>
             <Heading className={!displayName ? 'text-gray-500 italic' : ''}>{displayName || 'No display name'}</Heading>
             <a className="underline text-green-500" href={`mailto:${email}`}>{email}</a>
           </Box>
         </Box>
-        <Heading size="lg" className="mt-3">Total User Stats</Heading>
         {record.uid && !isLoading ? (
           <UserStat
             uid={record.uid}
