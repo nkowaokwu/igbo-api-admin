e8255bc135b6e529637e75b04e55cceb5dd9cf04
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-02-28 09:17:00 -0500

    Fix dialectal variations bug in monthly and weekly stats

--- src/Core/Dashboard/components/LacunaProgress/LacunaProgress.tsx
+++ src/Core/Dashboard/components/LacunaProgress/LacunaProgress.tsx
@@ -5,7 +5,7 @@ import moment from 'moment';
 import { DIALECTAL_VARIATIONS_GOAL, EXAMPLE_SENTENCES_GOAL } from 'src/Core/constants';
 import LinearProgressCard from '../LinearProgressCard';
 
-const MONTHS_LEFT = moment().diff(moment([2023, 6, 1]), 'months', true) || 1;
+const MONTHS_LEFT = Math.abs(moment().diff(moment([2023, 6, 1]), 'months', true) || 1);
 const TEAM_MEMBERS = 4;
 const WEEKLY = 4;
 const LacunaProgress = ({
