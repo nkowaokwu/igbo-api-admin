628d84ba4107c9c8bcf6f281b4ca3e146dab9045
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-06-10 23:26:49 -0700

    Add new fields for crowdsourcing and suggestion enums

--- src/backend/shared/constants/CrowdsourcingType.ts
+++ src/backend/shared/constants/CrowdsourcingType.ts
@@ -3,6 +3,7 @@ enum CrowdsourcingType {
   REVIEW_IGBO_DEFINITION = 'review_igbo_definition',
   RECORD_EXAMPLE_AUDIO = 'record_example_audio',
   VERIFY_EXAMPLE_AUDIO = 'verify_example_audio',
+  VERIFY_EXAMPLE_TEXT = 'verify_example_text',
 };
 
 export default CrowdsourcingType;
