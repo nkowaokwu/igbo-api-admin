9a03cad0826ae57cd89ab4dd8315a02023a488df
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-06-14 22:48:41 -0700

    Lazy load routes to cut JS bundle size down by half

--- src/App/withLastRoute.tsx
+++ src/App/withLastRoute.tsx
@@ -4,18 +4,24 @@ import LocalStorageKeys from 'src/shared/constants/LocalStorageKeys';
 import { hasTranscriberPermissions } from 'src/shared/utils/permissions';
 
 const LOGIN_HASH = '#/login';
-const withLastRoute = (Component: (value: any) => ReactElement) => (props: any): ReactElement => {
-  const permissions = usePermissions();
-  /* Skips saving the last route if the user is a transcriber */
-  /* This helps prevents transcribers getting stuck on unauthed routes */
-  const skipSavingLastRoute = hasTranscriberPermissions(permissions, true) || false;
-  useEffect(() => {
-    if (window.location.hash !== LOGIN_HASH && !skipSavingLastRoute) {
-      localStorage.setItem(LocalStorageKeys.REDIRECT_URL, window.location.hash);
-    }
-  }, [window.location.hash]);
+const withLastRoute =
+  (Component: (value: any) => ReactElement) =>
+  (props: any): ReactElement => {
+    const permissions = usePermissions();
+    /* Skips saving the last route if the user is a transcriber */
+    /* This helps prevents transcribers getting stuck on unauthed routes */
+    const skipSavingLastRoute = hasTranscriberPermissions(permissions, true) || false;
+    useEffect(() => {
+      if (window.location.hash !== LOGIN_HASH && !skipSavingLastRoute) {
+        localStorage.setItem(LocalStorageKeys.REDIRECT_URL, window.location.hash);
+      }
+    }, [window.location.hash]);
 
-  return <Component {...props} />;
-};
+    return (
+      <React.Suspense fallback={() => <p>Loading...</p>} {...props}>
+        <Component {...props} />
+      </React.Suspense>
+    );
+  };
 
 export default withLastRoute;
