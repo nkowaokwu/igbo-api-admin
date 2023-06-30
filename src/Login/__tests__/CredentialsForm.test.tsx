ed18861a6128ca105243097ddac5e54378a38d4e
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-06-30 00:59:26 -0400

    Changes the credential form to use the term sign up

--- src/Login/__tests__/CredentialsForm.test.tsx
+++ src/Login/__tests__/CredentialsForm.test.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import TestContext from 'src/__tests__/components/TestContext';
 import CredentialsForm from '../CredentialsForm';
 
 describe('CredentialsForm', () => {
   it('renders the entire login form', async () => {
     const { findByText, findByPlaceholderText } = render(
-      <CredentialsForm />,
+      <TestContext>
+        <CredentialsForm />
+      </TestContext>,
     );
+    await findByText('Log in to your account');
     await findByText('Email address');
     await findByPlaceholderText('Email address');
     await findByText('Password');
     await findByPlaceholderText('Password');
     await findByText('Forgot password?');
     await findByText('Sign in');
-    await findByText('Don\'t have an account?');
+    await findByText("Don't have an account?");
     await findByText('Create account');
     await findByText('Sign in with Google');
   });
