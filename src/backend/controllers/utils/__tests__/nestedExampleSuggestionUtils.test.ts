69a83b952b4c805227bd2aa1cf05f6cd77058bfd
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-08-03 00:20:24 -0400

    Handle archived example sentences

--- src/backend/controllers/utils/__tests__/nestedExampleSuggestionUtils.test.ts
+++ src/backend/controllers/utils/__tests__/nestedExampleSuggestionUtils.test.ts
@@ -1,17 +1,28 @@
 import { cloneDeep } from 'lodash';
+import { connectDatabase } from 'src/backend/utils/database';
+import { dropMongoDBCollections } from 'src/__tests__/shared';
+import { suggestNewExample, createExample, suggestNewWord, createWord } from 'src/__tests__/shared/commands';
+import { AUTH_TOKEN } from 'src/__tests__/shared/constants';
+import { exampleSuggestionData, wordSuggestionData } from 'src/__tests__/__mocks__/documentData';
 import { ExampleClientData, WordClientData } from '../interfaces';
-import { createNestedExampleSuggestionBody, getExamplesFromClientData } from '../nestedExampleSuggestionUtils';
+import {
+  createNestedExampleSuggestionBody,
+  getExamplesFromClientData,
+  assignExampleSuggestionToExampleData,
+} from '../nestedExampleSuggestionUtils';
 
 const word: WordClientData = {
   word: 'word',
   authorId: 'authorId',
-  definitions: [{
-    wordClass: 'NNC',
-    definitions: [],
-    igboDefinitions: [],
-    nsibidi: '',
-    nsibidiCharacters: [],
-  }],
+  definitions: [
+    {
+      wordClass: 'NNC',
+      definitions: [],
+      igboDefinitions: [],
+      nsibidi: '',
+      nsibidiCharacters: [],
+    },
+  ],
   pronunciation: '',
   examples: [
     // @ts-expect-error
@@ -35,6 +46,9 @@ const clientData: ExampleClientData = {
 };
 
 describe('nestedExampleSuggestionUtils', () => {
+  beforeEach(async () => {
+    await dropMongoDBCollections();
+  });
   it('creates a new example with example client data with user as author', () => {
     const createdExample = createNestedExampleSuggestionBody({
       example: clientData,
@@ -85,5 +99,45 @@ describe('nestedExampleSuggestionUtils', () => {
     const examples = getExamplesFromClientData(testWord);
     expect(examples[0].originalExampleId).toEqual('originalExampleId');
     expect(examples[0].authorId).toEqual('authorId');
+  });
+
+  it('assignExampleSuggestionToExampleData_success', async () => {
+    const mongooseConnection = await connectDatabase();
+    const wordSuggestionRes = await suggestNewWord(wordSuggestionData);
+    expect(wordSuggestionRes.status).toEqual(200);
+    const mergedWordRes = await createWord(wordSuggestionRes.body.id);
+    expect(mergedWordRes.status).toEqual(200);
+    const res = await suggestNewExample({ ...exampleSuggestionData, associatedWords: [mergedWordRes.body.id] });
+    expect(res.status).toEqual(200);
+
+    const exampleRes = await assignExampleSuggestionToExampleData({
+      wordSuggestion: { ...wordSuggestionData, id: wordSuggestionRes.body.id },
+      originalWord: { ...wordSuggestionData, id: mergedWordRes.body.id, toObject: jest.fn() },
+      mergedBy: AUTH_TOKEN.ADMIN_AUTH_TOKEN,
+      mongooseConnection,
+    });
+    expect(exampleRes.length).toEqual(0);
+  });
+
+  it('assignExampleSuggestionToExampleData_failure', async () => {
+    const mongooseConnection = await connectDatabase();
+    const wordSuggestionRes = await suggestNewWord(wordSuggestionData);
+    expect(wordSuggestionRes.status).toEqual(200);
+    const mergedWordRes = await createWord(wordSuggestionRes.body.id);
+    expect(mergedWordRes.status).toEqual(200);
+    const res = await suggestNewExample({ ...exampleSuggestionData, associatedWords: [mergedWordRes.body.id] });
+    expect(res.status).toEqual(200);
+    const mergingExampleSuggestion = { ...res.body, ...exampleSuggestionData };
+    const result = await createExample(mergingExampleSuggestion.id);
+    expect(result.status).toEqual(200);
+
+    await assignExampleSuggestionToExampleData({
+      wordSuggestion: { ...wordSuggestionData, id: wordSuggestionRes.body.id },
+      originalWord: { ...wordSuggestionData, id: mergedWordRes.body.id, toObject: jest.fn() },
+      mergedBy: AUTH_TOKEN.ADMIN_AUTH_TOKEN,
+      mongooseConnection,
+    }).catch((err) => {
+      console.log(err.message);
+    });
   });
 });
--- src/backend/controllers/utils/nestedExampleSuggestionUtils.ts
+++ src/backend/controllers/utils/nestedExampleSuggestionUtils.ts
@@ -173,7 +173,7 @@ export const assignExampleSuggestionToExampleData = async ({
   originalWord: Interfaces.Word;
   mergedBy: string;
   mongooseConnection: Connection;
-}): Promise<void> => {
+}): Promise<Interfaces.Example[]> => {
   const ExampleSuggestion = mongooseConnection.model('ExampleSuggestion', exampleSuggestionSchema);
   const Example = mongooseConnection.model('Example', exampleSchema);
 
@@ -185,7 +185,7 @@ export const assignExampleSuggestionToExampleData = async ({
   // 2. Get all word's nested examples
   const examples: Interfaces.Example[] = await Example.find({ associatedWords: originalWord.id.toString() });
 
-  if (examples.length > exampleSuggestions.length) {
+  if (examples.filter((example) => !example.archived).length > exampleSuggestions.length) {
     throw new Error(
       'There are less Example sentences than Example Suggestions. ' +
         'Please provide either equal to or more than the number of Example sentences.',
@@ -204,7 +204,7 @@ export const assignExampleSuggestionToExampleData = async ({
     await archiveExamples({ exampleIds: archivingExampleIds, mongooseConnection });
   }
 
-  await Promise.all(
+  return Promise.all(
     map(exampleSuggestions, async (exampleSuggestion: Interfaces.ExampleSuggestion) => {
       const removeSuggestionAssociatedIds: Interfaces.ExampleSuggestion = assign(exampleSuggestion);
       /* Before creating new Example from ExampleSuggestion,
