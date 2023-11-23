957b6ac5d7379e7f890408366a24bbfee28ce060
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-11-22 23:08:19 -0800

    Step through each month to see relevant stats

--- src/Core/Dashboard/components/UserStat/UserStat.tsx
+++ src/Core/Dashboard/components/UserStat/UserStat.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useEffect, useState } from 'react';
-import { Box, Heading, Skeleton } from '@chakra-ui/react';
+import { Box, Heading, Skeleton, useToast } from '@chakra-ui/react';
 import { usePermissions } from 'react-admin';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { hasCrowdsourcerPermission } from 'src/shared/utils/permissions';
@@ -27,36 +27,40 @@ const UserStat = ({
   completeExamples: number;
 }): ReactElement => {
   const [userStats, setUserStats] = useState(null);
-  const [recordingStats, setRecordingStats] = useState({ recorded: -1, verified: -1, mergedRecorded: {} });
+  const [recordingStats, setRecordingStats] = useState({ recorded: {}, verified: {}, mergedRecorded: {} });
   const [audioStats, setAudioStats] = useState({ audioApprovalsCount: 0, audioDenialsCount: 0 });
+  const [isLoading, setIsLoading] = useState(true);
   const { permissions } = usePermissions();
+  const toast = useToast();
   const isCrowdsourcer = hasCrowdsourcerPermission(permissions, true);
 
   useEffect(() => {
     (async () => {
-      const { uid } = user;
-      network(`/stats/users/${uid}`).then(({ json }) => setUserStats(json));
-      network(`/stats/users/${uid}/audio`).then(({ json }) => {
-        setAudioStats(json);
-      });
-      const { timestampedExampleSuggestions: mergedExampleSuggestion } = await getTotalMergedRecordedExampleSuggestions(
-        uid,
-      );
-      const { timestampedRecordedExampleSuggestions } = await getTotalRecordedExampleSuggestions(uid);
-      const { timestampedReviewedExampleSuggestions } = await getTotalReviewedExampleSuggestions(uid);
-      const recordedExampleSuggestions = Object.values(timestampedRecordedExampleSuggestions).reduce(
-        (total, count) => total + count,
-        0,
-      );
-      const reviewedExampleSuggestions = Object.values(timestampedReviewedExampleSuggestions).reduce(
-        (total, count) => total + count,
-        0,
-      );
-      setRecordingStats({
-        recorded: recordedExampleSuggestions,
-        verified: reviewedExampleSuggestions,
-        mergedRecorded: mergedExampleSuggestion, // Count of audio recordings by current user that have been merged
-      });
+      try {
+        const { uid } = user;
+        network(`/stats/users/${uid}`).then(({ json }) => setUserStats(json));
+        network(`/stats/users/${uid}/audio`).then(({ json }) => {
+          setAudioStats(json);
+        });
+        const { timestampedExampleSuggestions: mergedExampleSuggestion } =
+          await getTotalMergedRecordedExampleSuggestions(uid);
+        const { timestampedRecordedExampleSuggestions } = await getTotalRecordedExampleSuggestions(uid);
+        const { timestampedReviewedExampleSuggestions } = await getTotalReviewedExampleSuggestions(uid);
+        setRecordingStats({
+          recorded: timestampedRecordedExampleSuggestions,
+          verified: timestampedReviewedExampleSuggestions,
+          mergedRecorded: mergedExampleSuggestion, // Count of audio recordings by current user that have been merged
+        });
+        setIsLoading(false);
+      } catch (err) {
+        toast({
+          title: 'An error occurred',
+          description: 'Unable to load profile stats.',
+          status: 'error',
+          duration: 4000,
+          isClosable: true,
+        });
+      }
     })();
   }, []);
 
@@ -75,7 +79,7 @@ const UserStat = ({
           </Heading>
           <Box className="flex flex-col lg:flex-row justify-between items-start space-y-3 lg:space-y-0">
             <Box className="space-y-3 w-full">
-              <Skeleton isLoaded={recordingStats.recorded !== -1}>
+              <Skeleton isLoaded={!isLoading}>
                 <IgboSoundboxStats recordingStats={recordingStats} audioStats={audioStats} />
               </Skeleton>
               {!isCrowdsourcer ? <PersonalStats userStats={userStats} /> : null}
