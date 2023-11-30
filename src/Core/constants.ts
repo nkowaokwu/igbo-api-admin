3cc766d086a43fca009c600d98e23608445f0e54
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2023-11-30 10:12:57 -0500

    Update constants.ts

--- src/Core/constants.ts
+++ src/Core/constants.ts
@@ -41,49 +41,5 @@ export const BULK_UPLOAD_LIMIT = 500;
 
 /** Calculating payment prices */
 export const PRICE_PER_VERIFIED_RECORDING = 0.02;
-/* eslint-disable max-len */
-export const IGBO_API_VOLUNTEER_HOME_BASE = 'https://editor.igboapi.com/gettingstarted';
-export const DICTIONARY_EDITING_STANDARDS_URL = 'https://editor.igboapi.com/checklist';
-export const DICTIONARY_APP_URL = 'https://nkowaokwu.com';
-export const TWITTER_APP_URL = 'https://twitter.com/nkowaokwu/status';
-
-export const BUG_REPORT_URL = 'https://notionforms.io/forms/bug-reports-llbpnk';
-export const FEATURE_REQUEST_FORM_URL =
-  'https://notionforms.io/forms/request-a-new-igbo-api-editor-platform-feature-feature-9j8d2w';
-export const EDITORS_ONBOARDING_URL =
-  'https://charmed-sycamore-9e5.notion.site/Lexicographer-Onboarding-6acda527ced6412087890eaeee7935c5?pvs=4';
-
-export const VOICE_OVER_RECORDERS_SLACK_CHANNEL = 'https://igboapi.slack.com/archives/C01U6F9HG4T';
-export const EDITORS_TRANSLATORS_SLACK_CHANNEL = 'https://igboapi.slack.com/archives/C01UZQDDJ73';
-export const SOFTWARE_ENGINEERS_SLACK_CHANNEL = 'https://igboapi.slack.com/archives/C01UCDVSP0U';
-export const CROWDSOURCING_SLACK_CHANNEL = 'https://igboapi.slack.com/archives/C057ZCT24M7';
-
-export const PARTS_OF_SPEECH_LIST =
-  'https://www.notion.so/charmed-sycamore-9e5/Igbo-API-Parts-of-Speech-13a1aab7b7b041f6b306add21feeef02?pvs=4';
-export const RECORDING_AUDIO_STANDARDS_DOC =
-  'https://charmed-sycamore-9e5.notion.site/Recording-Audio-Standards-399d1ba25f4648ee9682f7feaeeb796f?pvs=4';
-export const IGBO_DEFINITIONS_STANDARDS_DOC =
-  'https://charmed-sycamore-9e5.notion.site/Igbo-Definitions-Standards-28a94d7c4ef948e08ef56ef61c09e3cf?pvs=4';
-export const CONSTRUCTED_TERMS_REVIEW_DOC =
-  'https://charmed-sycamore-9e5.notion.site/Nk-wa-okwu-Newly-Constructed-Words-Review-Process-cccc6e79e0ee4bdab8d3a538f0cd559c?pvs=4';
-
-export const SUFFICIENT_WORDS_GOAL = 8100;
-export const COMPLETE_WORDS_GOAL = 8100;
-export const DIALECTAL_VARIATIONS_GOAL = 25000;
-export const HEADWORD_AUDIO_PRONUNCIATION_GOAL = 8100;
-export const WORDS_WITH_NSIBIDI_GOAL = 4000;
-export const WORD_SUGGESTIONS_WITH_NSIBIDI_GOAL = 1000;
-export const IS_STANDARD_IGBO_GOAL = 8100;
-export const IGBO_DEFINITIONS_GOAL = 500;
-export const EXAMPLE_SENTENCES_GOAL = 25000;
-export const PROVERBS_GOAL = 4000;
-export const TOTAL_EXAMPLE_AUDIO_GOAL = 2000;
-export const TOTAL_EXAMPLE_SUGGESTION_AUDIO_GOAL = 100;
-
-export const BULK_UPLOAD_LIMIT = 500;
-
-/** Calculating payment prices */
-export const PRICE_PER_VERIFIED_RECORDING = 0.02;
 export const PRICE_PER_RECORDING = 0.013;
 export const PRICE_PER_REVIEW = 0.013;
-
